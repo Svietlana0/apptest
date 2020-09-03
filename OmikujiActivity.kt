a04b7931df001da7cd333bc3b7897e4757bc5bb8
Akaritanaka <70252160+Svietlana0@users.noreply.github.com>
2020-09-03 10:16:44 +0900

    Add files via upload

--- OmikujiActivity.kt
+++ OmikujiActivity.kt
@@ -7,5 +7,18 @@ class OmikujiActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+    //文字を表示
+        val str = "大吉"
+        hello_view.text = str
+        val rnd = Ranom()
+        val number = rnd.nextInt(3)
+        hello_view.text = "$str 乱数 ${number + 1}"
+        if( number == 0) {
+            str = "吉"
+        } else if (number == 1){
+            str = "吉"}
+
+
+        }
     }
-}
+    }
